1a301074507a3a70aa8683dd53df4054599e83e1
Nuddle <lucas.bretel@hotmail.fr>
2020-07-07 15:54:29 +0200

    correctifs

--- src/test/java/com/github/dorval/francois/composant/MainTest.java
+++ src/test/java/com/github/dorval/francois/composant/MainTest.java
@@ -17,7 +17,7 @@ public class MainTest {
 
         Main.main(new String[]{"6", "2"});
 
-        Assert.assertEquals("6/2=3\n", bos.toString());
+        Assert.assertEquals("6/2=3\r\n", bos.toString());
 
         System.setOut(originalOut);
     }
@@ -25,5 +25,10 @@ public class MainTest {
     @Test(expected = IllegalArgumentException.class)
     public void testCasKo() {
         Main.main(new String[]{"abcd"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivisionZero() {
+        Main.main(new String[]{"10", "0"});
     }
 }
